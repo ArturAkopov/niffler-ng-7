f43f114a2189641d06beebe135282fd258e16b03
Artur <116442638+ArturAkopov@users.noreply.github.com>
2025-06-07 20:56:05 +0300

    Kafka + Тест

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/tests/kafka/KafkaTests.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/tests/kafka/KafkaTests.java
@@ -4,6 +4,9 @@ import guru.qa.niffler.api.AuthApi;
 import guru.qa.niffler.api.core.RestClient;
 import guru.qa.niffler.api.core.ThreadSafeCookieStore;
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.data.entity.userdata.UserEntity;
+import guru.qa.niffler.data.repository.UserdataUserRepository;
+import guru.qa.niffler.data.repository.impl.UserdataUserRepositorySpringJdbc;
 import guru.qa.niffler.jupiter.annotation.meta.KafkaTest;
 import guru.qa.niffler.model.rest.UserJson;
 import guru.qa.niffler.service.KafkaService;
@@ -12,12 +15,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Optional;
+
+import static guru.qa.niffler.model.CurrencyValues.RUB;
 
 @KafkaTest
 public class KafkaTests {
 
     private static final Config CFG = Config.getInstance();
     private final AuthApi authApi = new RestClient.EmtyRestClient(CFG.authUrl()).create(AuthApi.class);
+    private final UserdataUserRepository repository = new UserdataUserRepositorySpringJdbc();
 
     @Test
     void userShouldBeProducedToKafka() throws IOException, InterruptedException {
@@ -25,6 +32,7 @@ public class KafkaTests {
         final String username = RandomDataUtils.randomUsername();
         final String password = "12345";
 
+        authApi.requestRegisterForm().execute();
         authApi.register(
                 username,
                 password,
@@ -36,6 +44,30 @@ public class KafkaTests {
         Assertions.assertEquals(
                 username,
                 userFromKafka.username()
+        );
+    }
+
+    @Test
+    void userShouldBeAddedInDbAfterProducedToKafka() throws IOException, InterruptedException {
+
+        final String username = RandomDataUtils.randomUsername();
+        final String password = "12345";
+
+        authApi.requestRegisterForm().execute();
+        authApi.register(
+                username,
+                password,
+                password,
+                ThreadSafeCookieStore.INSTANCE.cookieValue("XSRF-TOKEN")
+        ).execute();
+
+        UserJson userFromKafka = KafkaService.getUserJson(username);
+        Optional<UserEntity> userFromDb = repository.findByUsername(userFromKafka.username());
+
+        Assertions.assertEquals(username, userFromDb.get().getUsername());
+        Assertions.assertEquals(
+                RUB.name(),
+                userFromDb.get().getCurrency().name()
         );
     }
 }
